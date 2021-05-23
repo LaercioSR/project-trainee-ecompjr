d051df9f1c7f6059432950734d2ad2f52bb28951
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2021-05-23 20:37:35 -0300

    :sparkles: Added about ecomp jr

---
+++ resources/views/layouts/ecompjr.blade.php
@@ -0,0 +1,52 @@
+<div class="overflow-x-auto bg-blue-300">
+    <div class="min-w-screen min-h-screen flex justify-center font-sans overflow-hidden">
+        <div class="w-full lg:w-5/6">
+            <div class="flex justify-center mt-12">
+                <img src="{{ asset('img/logoecompjr.png') }}" alt="Logo da Ecomp Jr." style="max-width: 20%; height: auto; "/>
+            </div>
+            <div class="flex justify-center">
+                <p style="font-weight: bold">EMPRESA JÚNIOR DE ENGENHARIA DE COMPUTAÇÃO DA UEFS</p>
+            </div>
+
+            <div class="flex justify-center mt-12 text-gray-700">
+                <p>Projeto Desenvolvido como parte do programa trainee da Ecomp Jr.</p>
+            </div>
+
+            <div class="mt-12 mr-48 ml-48 text-justify">
+                <h2 class="text-2xl font-bold">SOBRE A ECOMP JR:</h2>
+                <div class="text-lg	justify-center text-gray-900">
+                    <p>
+                        Empresa Júnior de Engenharia de Computação, composta por alunos do curso da Universidade Estadual de Feira de Santana, que promove impactos positivos em micro e pequenas empresas por meio de soluções tecnológicas.
+                    </p>
+                    <p>
+                        Fundada em 2007, a EcompJr atualmente participa ativamente do propósito que envolve ser Movimento Empresa Júnior. A empresa promove uma vivência empresarial estratégica para estudantes interessados em liderar a mudança no atual cenário brasileiro.
+                    </p>
+                </div>
+                <div class="mt-4">
+                    <h3 class="text-1xl font-bold">MISSÃO:</h3>
+                    <span>
+                        Através da vivência empresarial e trabalho em equipe, formar profissionais inovadores, comprometidos e capazes de vencer desafios futuros.
+                    </span>
+                    <h3 class="text-1xl font-bold">VISÃO:</h3>
+                    <span>
+                        Ser uma empresa que realiza projetos de impacto, mais conectada ao movimento empresa júnior e com grande reconhecimento dentro e fora da universidade.
+                    </span>
+                    <h3 class="text-1xl font-bold">VALORES:</h3>
+                    <span>
+                        Protagonismo, Cooperatividade, Assiduidade, Resiliência, Constância em resultados, Evolução com os erros, Conexão com o Movimento, Foco na Qualidade, Ética e Empatia.
+                    </span>
+                </div>
+                <div class="mt-10 mb-10">
+                    <h3 class="text-1xl font-bold">Serviços Oferecidos:</h3>
+                    <ul class="list-disc">
+                        <li>Sites</li>
+                        <li>Sistemas - Locais</li>
+                        <li>Aplicativos Mobile</li>
+                        <li>Sistema Web</li>
+                        <li>eCommerce</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
